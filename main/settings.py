7519b754f8effb285a3d12be516586e43968b97c
Daniel Camilo Tirano Florez <124839106+XfeRYT@users.noreply.github.com>
2023-12-15 21:30:16 -0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'formtools',
     'dbbackup',
     'backup',
+    'backup.apps.BackupConfig',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS= "bootstrap5"
 CRISPY_TEMPLATE_PACK= "bootstrap5"

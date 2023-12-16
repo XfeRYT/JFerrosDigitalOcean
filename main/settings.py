b53db9adaf509037d749c29ea332035225fe95c1
Daniel Camilo Tirano Florez <124839106+XfeRYT@users.noreply.github.com>
2023-12-15 21:21:01 -0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+
+if 'DIGITALOCEAN' in os.environ:
+    DISABLE_COLLECTSTATIC = '1'
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
